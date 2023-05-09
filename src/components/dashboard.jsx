a97d1e147afc793e4545967c78149f89a61c7fa0
Tharindu N <tharindu.nm@yahoo.com>
2023-05-09 12:40:50 +0530

    removed all components

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,60 +1,9 @@
 import React from "react";
-import styled from "styled-components";
-
-import CheckoutActions from "./dashboard/checkoutActions";
-import NavBar from "./navBar";
-
-const DashboardStyle = styled.div`
-	.App {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	.navbar {
-		position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-		height: 5%;
-		display: flex;
-		z-index: 1;
-	}
-
-	.main-content {
-		flex: 1;
-		padding: 1rem;
-	}
-
-	.checkout-actions {
-		position: fixed;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		width: 100%;
-		height: 12%;
-		display: flex;
-		padding: 1rem;
-		z-index: 1;
-	}
-`;
 
 const Dashboard = () => {
 	return (
 		<React.Fragment>
-			<DashboardStyle>
-				<div className="App">
-					<div className="navbar">
-						<NavBar />
-					</div>
-					<div className="main-content">
-						{/* your other components here */}
-					</div>
-					<div className="checkout-actions">
-						<CheckoutActions />
-					</div>
-				</div>
-			</DashboardStyle>
+			<h1>Dashboard</h1>
 		</React.Fragment>
 	);
 };
